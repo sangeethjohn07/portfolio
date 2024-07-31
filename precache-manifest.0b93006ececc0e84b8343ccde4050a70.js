c7b8f1d879f316a2c055b3ad1f19d90f35a95343
sangeethjohn07 <sangeethjohn1997@gmail.com>
2024-07-31 16:43:01 +0530

    Updates

---
+++ precache-manifest.0b93006ececc0e84b8343ccde4050a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde5a71b30bfef09da0e5cd051b1febc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9ac9090931b75d61406",
+    "url": "/portfolio/static/css/main.28a42f1a.chunk.css"
+  },
+  {
+    "revision": "761318a94a9efa292667",
+    "url": "/portfolio/static/js/2.6c19ab7b.chunk.js"
+  },
+  {
+    "revision": "c06378aac27e04c4ecda07d0d952ad9c",
+    "url": "/portfolio/static/js/2.6c19ab7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ac9090931b75d61406",
+    "url": "/portfolio/static/js/main.a3bb1de9.chunk.js"
+  },
+  {
+    "revision": "812227353ecd34e309c1",
+    "url": "/portfolio/static/js/runtime-main.d5982813.js"
+  }
+]);
